ac8d3995bc341266f8626cbe588f281866c3a19e
BradleyHollis <bhollis@dev-10.com>
2023-03-01 17:19:43 -0500

    Delete method not allowed on dishes is implemented

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -6,9 +6,11 @@ const methodNotAllowed = require('../errors/methodNotAllowed');
 router.route('/')
     .get(controller.list)
     .post(controller.create)
+    .all(methodNotAllowed)
 
 router.route('/:dishId')
     .get(controller.read)
     .put(controller.update)
+    .all(methodNotAllowed)
 
 module.exports = router;
