b676d57e5d6dbcebea9b8d369273591335647010
BradleyHollis <bhollis@dev-10.com>
2023-03-01 17:32:00 -0500

    Order get operation is working as expected

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,30 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+function list(req, res){
+    res.json({ data: orders });
+}
+
+function orderExists(req, res, next){
+    const { orderId } = req.params; 
+    const foundOrder = orders.find(({ id }) => id == orderId );
+    
+    if(foundOrder){
+        res.locals.order = foundOrder; 
+        return next();
+    }
+
+    next({
+        status: 404, 
+        message: `Order id not found: ${orderId}`
+    })
+}
+
+function read(req, res){
+    res.json({ data: res.locals.order });
+}
+
+module.exports = {
+    list, 
+    read: [orderExists, read],
+}
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
+const controller = require('./orders.controller');
 
 // TODO: Implement the /orders routes needed to make the tests pass
+router.route('/')
+    .get(controller.list)
+
+router.route('/:orderId')
+    .get(controller.read)
 
 module.exports = router;
